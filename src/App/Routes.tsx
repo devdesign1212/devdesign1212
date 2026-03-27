0ad9102476592b19ee30532206534b24d895c310
devdesign1212 <dev.design1212@gmail.com>
2026-03-27 21:42:51 +0530

    feat: removed unused code

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -1,17 +1,17 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from '@/Pages/Home';
 import LaboratoryGallery from '@/Pages/Home/LaboratoryGallery';
 import MainLayout from '@/Pages/Home/MainLayout';
-import { auth } from '@/Services/firebase';
+// import { auth } from '@/Services/firebase';
 
 const RouterOutlet: React.FC = () => {
-  const [user, setUser] = useState<any>(null);
+  // const [user, setUser] = useState<any>(null);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(u => setUser(u));
-    return () => unsubscribe();
-  }, []);
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged(u => setUser(u));
+  //   return () => unsubscribe();
+  // }, []);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
